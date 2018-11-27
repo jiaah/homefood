edc986f8d8dbdb854a6731c0b3fae754b8295055
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-11-27 16:04:13 +0900

    add favicon, manifest files to dist

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 /* --- Components --- */
 import Loader from './src/shared/loader';
 // import './firebase/firebase';
+import './styles/main.scss';
 
 const Nav = Loader({
   loader: () => import('./src/components/nav' /* webpackChunkName: 'Nav' */),
