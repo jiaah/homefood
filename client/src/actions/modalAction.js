76854f69c0210e18354936df81396a1eff0e5cc6
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-12-02 22:45:47 +0900

    modified home main contents

--- client/src/actions/modalAction.js
+++
@@ -1,9 +0,0 @@
-import * as types from './actionTypes';
-
-export const hideModalAction = () => ({
-  type: types.HIDE_MODAL,
-});
-
-export const showModalAction = () => ({
-  type: types.SHOW_MODAL,
-});
--- client/src/shared/lazyload.js
+++
@@ -1,3 +0,0 @@
-export default {
-  element_selector: '.lazy',
-};
